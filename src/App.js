3f122ef46de97ebf5599e490ba38ee0f506cd65d
Isak Hauknes <isakhauknes@gmail.com>
2022-01-28 14:28:15 +0100

    FEAT: Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TranslationPage from './Components/Views/TranslationPage';
+import NotFound from "./Components/Views/NotFound";
+import StartupPage from "./Components/Views/StartupPage";
+import TranslationPage from "./Components/Views/TranslationPage";
+import ProfilePage from "./Components/Views/ProfilePage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<StartupPage />} />
+					<Route path="/translation" element={<TranslationPage />} />
+					<Route path="/profile" element={<ProfilePage />} />
+					<Route path='*' element={<NotFound/>} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/Views/TranslationPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TranslationPage = () => {
+  return <div></div>;
+};
+
+export default TranslationPage;
