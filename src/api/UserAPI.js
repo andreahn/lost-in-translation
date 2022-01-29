3863eb4b49f4188419931c66c7df96819642b4ab
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-01-29 12:29:27 +0100

    refactor: changed API functions to async

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -1,33 +1,35 @@
 const apiURL = "https://trivia-vue-api.herokuapp.com"
 const apiKey = "Hr0Q8UeUONO66F3WZzW5wgmK0XFcE4GJwKL5kMwkG2y5MJ4XSNxOQvZZzWMxeOhk"
 
-export function apiFetchAllUsers() {
+export async function apiFetchAllUsers() {
     // fetches all users from API
 
-    fetch(`${apiURL}/translations`)
-        .then(response => response.json())
-        .then(results => {
-            return [null, results]
-        })
-        .catch(error => {
-            return [error.message,[]]
-        })
+    try {
+        const response = await fetch(`${apiURL}/translations`)
+        const data = await response.json()
+
+        return [null, data]
+    }
+    catch (e){
+        return [e.message, []]
+    }
 }
 
-export function apiFetchUser(username){
+export async function apiFetchUser(username){
     //fetches user(s) with a given username
 
-    fetch(`${apiURL}/translations?username=${username}`)
-        .then(response => response.json())
-        .then(results => {
-            return [null, results]
-        })
-        .catch(error => {
-            return [error.message,[]]
-        })
+    try {
+        const response = await fetch(`${apiURL}/translations?username=${username}`)
+        const data = await response.json()
+
+        return [null, data]
+    }
+    catch (e){
+        return [e.message, []]
+    }
 }
 
-export function createNewUser(username){
+export async function createNewUser(username){
     // creates a new user given a username
     // returns new user object if successful
 
@@ -43,22 +45,18 @@ export function createNewUser(username){
         })
     }
 
-    fetch(`${apiURL}/translations`, requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Could not create new user')
-            }
-            return response.json()
-        })
-        .then(newUser => {
-            return [null, newUser]
-        })
-        .catch(error => {
-            return [error.message, []]
-        })
+    try {
+        const response = await fetch(`${apiURL}/translations`, requestOptions)
+        const data = await response.json()
+
+        return [null, data]
+    }
+    catch (e){
+        return [e.message, []]
+    }
 }
 
-export function addTranslationToUser (userID, translations){
+export async function addTranslationToUser (userID, translations){
     // adds translations to user (given user ID, not username)
     // returns updated user object
 
@@ -73,18 +71,14 @@ export function addTranslationToUser (userID, translations){
         })
     }
 
-    fetch(`${apiURL}/translations/${userID}`, requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Could not update translations history')
-            }
-            return response.json()
-        })
-        .then(updatedUser => {
-            return [null, updatedUser]
-        })
-        .catch(error => {
-            return [error.message, []]
-        })
 
+    try {
+        const response = await fetch(`${apiURL}/translations/${userID}`, requestOptions)
+        const data = await response.json()
+
+        return [null, data]
+    }
+    catch (e){
+        return [e.message, []]
+    }
 }
