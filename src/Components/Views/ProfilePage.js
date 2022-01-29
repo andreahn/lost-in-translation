cc4f730162b1876a9fa6a6132b4c7a881926a288
Stian Økland <sok003@post.uit.no>
2022-01-29 13:32:33 +0100

    FEAT: Fetch data about one user WIP

--- src/Components/Views/ProfilePage.js
+++ src/Components/Views/ProfilePage.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUserContext } from '../../context/UserContext';
+import { apiFetchUser } from '../../api/UserAPI';
 
 const ProfilePage = () => {
-  const [user, setUser] = useUserContext()
+  // const [user, setUser] = useUserContext()
+  const [user, setUser] = useState({})
+
+
+  const getUserInfo = async () => {
+    const userInfo = await apiFetchUser('dewaldels')
+    setUser(userInfo)
+    console.log(userInfo)
+
+  }
+  
+  useEffect(() => {
+    getUserInfo()
+  }, [])
 
   return (
-    <>On profile page: {user.username} </>
+    // <>On profile page: {user.username} </>
+    <main>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </main>
     );
 };
 
